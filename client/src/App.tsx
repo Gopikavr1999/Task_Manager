79967a5eaecb309275ec611cfaafb989e850d56e
Gopika V R <gopikavr1712@gmail.com>
2025-12-01 14:48:06 +0530

    5th commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
-
+import { Route, Routes } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 function App() {
 
   return (
-    <>
-      hi
-      
-    </>
+    <Routes>
+      <Route path='/register' element={<Register/>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/dashboard' element={<Dashboard/>} />
+    </Routes>
   )
 }
 
